2c9521b7e76c3914dd0bf259edfaefb5fa786874
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-26 13:33:36 +0100

    Update button.js

--- client/alternativeElements/button.js
+++ client/alternativeElements/button.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: props.width ? props.width : "400px",
+      onClick: props.onClick
+    }
+  }
   render() {
     return (
       <React.Fragment>
-        <button>
+        <button style={{width: this.state.width}} onClick={() => {
+          this.setState({width: "0"});
+          this.state.onClick();
+          this.setState({onClick: this.props.onClick, width: props.width ? props.width : "400px"});
+        }}>
           {this.props.children}
         </button>
       </React.Fragment>
