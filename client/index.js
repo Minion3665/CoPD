b11e3e59abbc44d0cb3ff0b69aaad16fe7921f12
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-26 10:57:59 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -14,20 +14,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <button style={{
-          backgroundColor: "#28c2ff",
-          color: "#173753",
-          width: "400px",
-          height: "100px",
-          boxShadow: "0 0 50px 15px #2191fb",
-          borderRadius: "100px",
-          maxWidth: "80vw",
-          transition: "all 1s",
-          overflow: "hidden",
-          position: "relative",
-          left: "50%",
-          transform: "translate(-50%, 0)"
-        }}>
+        <button>
           Example Button
         </button>
       </React.Fragment>
