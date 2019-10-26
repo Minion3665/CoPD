e22dce9bbb0396b7d05ea60caef8f4b734ca343b
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-26 12:05:15 +0100

    Update button.js

--- client/alternativeElements/button.js
+++ client/alternativeElements/button.js
@@ -1,1 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
+class Button extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <button>
+          {this.props.children}
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Button;
