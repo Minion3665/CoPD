3395c71b2a3913dc3be98b8459a194bba0611e30
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-27 08:39:56 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -25,7 +25,9 @@ class App extends Component {
         }} visible={true}>
           Example Button
         </Button>
-        <Button returnfunctions={(functions) => {this.setState({showButtonTwo: functions.show})}} onClick={() => {
+        <Button returnfunctions={(functions) => {
+          this.setState({showButtonTwo: functions.show});
+        }} onClick={() => {
           alert("Example alert");
         }} visible={false}>
           Another Example Button
